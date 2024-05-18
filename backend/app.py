4333fe3d9961386dc2258462ab8a281f3c15e2e0
dorjeezzz <dj.namjyal@gmail.com>
2024-05-17 21:19:59 -0600

    Wrote tracked products class and init

--- backend/app.py
+++ backend/app.py
@@ -15,3 +15,30 @@ class ProductResult(db.Model):
     img = db.Column(db.String(1000))
     url = db.Column(db.String(1000))
     price = db.Column(db.Float)
+    created_at db.Column(db.DateTime, default = datetime.utcnow)
+    search_text = db.Column(db.String(255))
+    source = db.Column(db.String(255))
+
+    def __init__(self, name, img, url, price, search_text, source):
+        self.name = name
+        self.img = img
+        self.url = url
+        self.price = price
+        self.search_text = search_text
+        self.source = source
+
+class TrackedProducts(db.Model):
+    id = db.Column(db.Integer, primary_keys = True)
+    name = db.Column(db.String(1000))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    tracked = db.Column(db.Boolean, default=True)
+
+    def __init__(self, name, tracked=True):
+        self.name = name
+        self.tracked = tracked
+
+@app.route('/results', methods = ['POST'])
+def submit_results():
+    results = requests.json.get('data')
+    search_text = request.json.get("search_text")
+    source = request.json.get("source")
