2bed6ce7aed5ccbcd80081278b49028c9d10f8dc
dorjeezzz <dj.namjyal@gmail.com>
2024-05-25 20:46:27 -0600

    implemented scrapper folder with init.py using argv

---
+++ backend/scraper/__init__.py
@@ -0,0 +1,12 @@
+from main import main
+import sys 
+import asyncio
+
+if __name__ = '__main__':
+    if (len(sys.argv) < 3):
+        print("usage: python -m package_name url search_text endpoint")
+        sys.exit(1)
+    url = sys.arvg[1]
+    stxt = sys.argv[2]
+    endpoint = sys.argv[3]
+    asyncio.run(main(url, stxt, endpoint))
