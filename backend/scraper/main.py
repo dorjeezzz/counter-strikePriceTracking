64eb5a338f061b96784532daac965e352b50d3cd
dorjeezzz <dj.namjyal@gmail.com>
2024-05-28 09:53:55 -0600

    working on main.py for a public price scraper over csfloat, completed function to search and loadauth

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -1,5 +1,43 @@
-{
-    "host": "",
-    "username": "",
-    "password": ""
+import asyncio
+from playwright.async_api import async_playwright
+import json
+import os
+from amazon import get_product as get_amazon_product
+from requests import post
+
+FLOAT = "https://csfloat.com"
+
+URLS = {
+    FLOAT:{
+        "search_field_query": 'input[name="field-keywords"]',
+        "search_button_query": 'input[value="Go"]',
+        #get back to this step later
+    }
 }
+
+available_urls = URLS.keys()
+
+def load_auth():
+    FILE = os.path.join("Scraper","auth.json")
+    with open(FILE, 'r') as f: 
+        return json.load(f)
+
+cred = load_auth()
+auth = f'{cred["username"]}:{cred["password"]}'
+browser_url = f'wss://{auth}@{cred["host"]}'
+#should successfully take in credentials 
+async def search(metadata, page, search_text):
+    print(f"Looking for {search_text} on {page.url}")
+    search_field_query = metadata.get("search_field_query")
+    search_button_query = metadata.get("search_button_query")
+    if search_field_query and search_button_query:
+        print("Completing input field")
+        search_box = await page.wait_for_selector(search_field_query)
+        await search_box.type(search_text)
+        print("Pressing search button")
+        button = await page.wait_for_selector(search_button_query)
+        await button.click()
+    else: raise Exception("Error: Could not search. Please try again")
+
+    await page.wait_for_load_state()
+    return page
