790d01ba629a8a4f50cf39fec2bf48be3f88ea59
dorjeezzz <dj.namjyal@gmail.com>
2024-06-12 16:18:40 -0600

    created function to handle new search text and start csfloat scraper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,9 +27,19 @@ function App(){
         }
     };
 
-    // const handleSearchTextClick = async (searchText) => {
-        
-    // };
+    const handleSearchTextClick = async (searchText) => {
+        try{
+            const response = await axios.get(
+                `${URL}/results?search_text=${searchText}`
+            );
+            const data = response.data;
+            setPriceHistory(data);
+            setShowPriceHistory(true);
+        }
+        catch(error){
+            console.error("Error Fetching Price History, Try Again.", error);
+        }
+    };
 
     const handlePriceHistoryClose = () =>{
         setShowPriceHistory(false);
@@ -40,10 +50,32 @@ function App(){
         setNewSearchText(event.target.value);
     };
 
+    const handleNewSearchTextSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            await axios.post(`${URL}/start-scraper`,{
+                search_text: newSearchText,
+                url: "https://cs.float",
+            });
+            alert("Successfully Started");
+            setSearchTexts([...searchTexts, newSearchText]);
+            setNewSearchText("");
+        }
+        catch(error){
+            console.error("Error starting...", error);
+        }
+    };
     return(
         <div className = "main">
             <h1>Product Search Tool</h1>
-
+            <form onSubmit={handleNewSearchTextSubmit}>
+                <label>Search for New Item:</label>
+                <input
+                
+                />
+            </form>
         </div>
     );
 }
+
+export default App;
